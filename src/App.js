0c2e880714c6a99394d341ec1c230b48ef243d01
Shamika Earle <e.shamika@yahoo.com>
2021-08-03 19:57:33 -0400

    Feat: Add Home Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
     const title = 'Welcome to my Blog';
   return (
     <div className="App">
+        <NavBar/>
+        
       <div className="content">
           <h1> {title}</h1>
+          <Home/>
       </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const home = () => {
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+        </div>
+    );
+}
+
+export default home;
