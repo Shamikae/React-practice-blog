36a9e77f312c0723906f73a10101b95a8c946781
Shamika Earle <e.shamika@yahoo.com>
2021-08-03 22:38:46 -0400

    Lesson: Click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const home = () => {
+/* e for event object*/
+    const handleClick = (e) => {
+        console.log('Hello Ninjas', e);
+    }
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('Sy')}>Click me again</button> {/* Anonymus arrow function */}
         </div>
     );
 }
